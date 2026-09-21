37f2d6c2a6a7d20aa51af8e52e947bcad5b964df
abdulla bashir <abdullagozer@gmail.com>
2026-09-21 20:47:22 +0200

    feat: add Firebase repository and role demo foundation

--- lib/core/providers.dart
+++ lib/core/providers.dart
@@ -1,12 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../data/repositories/clinic_repository.dart';
 import '../data/models/models.dart';
+import '../data/repositories/clinic_repository.dart';
 
-final clinicRepositoryProvider =
-    Provider<ClinicRepository>((_) => DemoClinicRepository());
-final doctorsProvider = FutureProvider<List<Doctor>>(
-    (ref) => ref.watch(clinicRepositoryProvider).getDoctors());
-final servicesProvider = FutureProvider<List<MedicalService>>(
-    (ref) => ref.watch(clinicRepositoryProvider).getServices());
-final appointmentsProvider = FutureProvider<List<Appointment>>(
-    (ref) => ref.watch(clinicRepositoryProvider).getAppointments());
+final firebaseAvailableProvider=Provider<bool>((_)=>Firebase.apps.isNotEmpty);
+final clinicRepositoryProvider=Provider<ClinicRepository>((ref)=>Firebase.apps.isNotEmpty?FirebaseClinicRepository():DemoClinicRepository());
+final currentUserProvider=StreamProvider<AppUser?>((ref)=>ref.watch(clinicRepositoryProvider).watchCurrentUser());
+final doctorsProvider=FutureProvider<List<Doctor>>((ref)=>ref.watch(clinicRepositoryProvider).getDoctors());
+final servicesProvider=FutureProvider<List<MedicalService>>((ref)=>ref.watch(clinicRepositoryProvider).getServices());
+final appointmentsProvider=FutureProvider<List<Appointment>>((ref)=>ref.watch(clinicRepositoryProvider).getAppointments(patientId:ref.watch(clinicRepositoryProvider).currentUser?.id));
